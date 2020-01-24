09c1574cb71ef56a7d8f36e740a1c7968b9229d7
Julian END416 <marti994@uwm.edu>
2020-01-24 16:22:58 -0600

    LME Models: Bias + CI

--- 2_scripts/4_analysis.R
+++ 2_scripts/4_analysis.R
@@ -9,6 +9,7 @@ analysis_avsa(merged_list = list_merged)
 
 
 # means and sd ------------------------------------------------------------
+
 data_time <- read_csv(file = "./3_data/analysis/table_analysis_time.csv")
 data_percentage <- read_csv(file = "./3_data/analysis/table_analysis_percentage.csv")
 
@@ -37,7 +38,6 @@ colSd <- function(x, na.rm = T) {
 }
 
 avg_times <- colMeans(data_time)
-avg_times
 sd_times <- colSd(data_time)
 
 avg_percs <- colMeans(data_percentage)
@@ -162,93 +162,167 @@ names(sd_times) <- bind_names
 names(avg_percs) <- bind_names
 names(sd_percs) <- bind_names
 
-rbind(avg_times,
-      sd_times,
-      avg_percs,
-      sd_percs)
-
-# linear mixed effects model - bias and CI --------------------------------
-
-
-
-
+descriptive_table <- rbind(avg_times,
+                           sd_times,
+                           avg_percs,
+                           sd_percs)
+descriptive_table <- as.data.frame(descriptive_table)
+write.csv(descriptive_table,
+          file = "./4_results/visit_descriptives.csv")
+write_csv(descriptive_table,
+          path = "./4_results/visit_descriptives.csv",
+          col_names = T)
+write
+# linear mixed effects model - bias, SE, CI, means, sd --------------------------------
 
 # fit models: bias~b0 + b_i + e_ij
-sitmodel <- lmer(img_sitting - ap_sitting ~ 1 + (1|ID),data=data)
-standmodel <- lmer(img_standing - ap_standing ~ 1 + (1|ID),data=data)
-movemodel <- lmer(img_move - ap_move ~ 1 + (1|ID),data=data)
+sitmodel <- lmer(sit_anno - sit_ap ~ 1 + (1|ID),data = data_time)
+standmodel <- lmer(stand_anno - stand_ap ~ 1 + (1|ID),data = data_time)
+movemodel <- lmer(move_anno - move_ap ~ 1 + (1|ID),data = data_time)
 
 
 # make a table for the results:
-
-# rows are the postures
-summary.table <- data.frame(posture=c("sit","stand","move"))
+summary_table <- data.frame(posture = c("sit","stand","move"))
 
 # biases are estimated from model
-summary.table$bias <- c(fixef(sitmodel),fixef(standmodel),fixef(movemodel))
+summary_table$bias <- c(fixef(sitmodel),
+                        fixef(standmodel),
+                        fixef(movemodel))
 
 # sd is "unexplained variability" in the biases
-summary.table$sd <- c(as.data.frame(VarCorr(sitmodel))[2,5],
-							as.data.frame(VarCorr(standmodel))[2,5],
-							as.data.frame(VarCorr(movemodel))[2,5])
+VarCorr(sitmodel)
+as.data.frame(VarCorr(sitmodel))
+summary_table$sd <- c(as.data.frame(VarCorr(sitmodel))[2,5],
+                      as.data.frame(VarCorr(standmodel))[2,5],
+                      as.data.frame(VarCorr(movemodel))[2,5])
 
 # approximate 95% CIs
-summary.table$upper.95.bias <- summary.table$lower.95.bias <- NA
-summary.table[,4:5] <- rbind(confint(sitmodel,3),confint(standmodel,3),confint(movemodel,3))
-
+summary_table$upper_95_bias <- summary_table$lower_95_bias <- NA
+confint(sitmodel)
+confint(sitmodel, 3)
+summary_table[,4:5] <- rbind(confint(sitmodel,3),
+                             confint(standmodel,3),
+                             confint(movemodel,3))
 
 # raw means:
-summary.table$ap.min.mean <- c(mean(data$ap_sitting),mean(data$ap_standing),mean(data$ap_move))
-summary.table$ap.min.sd <- c(sd(data$ap_sitting),sd(data$ap_standing),sd(data$ap_move))
-summary.table$img.min.mean <- c(mean(data$img_sitting),mean(data$img_standing),mean(data$img_move))
-summary.table$img.min.sd <- c(sd(data$img_sitting),sd(data$img_standing),sd(data$img_move))
+summary_table$ap_min_mean <- c(mean(data_time$sit_ap),
+                               mean(data_time$stand_ap),
+                               mean(data_time$move_ap))
+summary_table$ap_min_sd <- c(sd(data_time$sit_ap),
+                             sd(data_time$stand_ap),
+                             sd(data_time$move_ap))
+summary_table$img_min_mean <- c(mean(data_time$sit_anno),
+                                mean(data_time$stand_anno),
+                                mean(data_time$move_anno))
+summary_table$img_min_sd <- c(sd(data_time$sit_anno),
+                              sd(data_time$stand_anno),
+                              sd(data_time$move_anno))
 
 # % sum table
+perc_sum_table <- summary_table
 
-perc.sum.table <- summary.table
-
-perc.sum.table$bias <- 100*summary.table$bias/summary.table$ap.min.mean
-perc.sum.table$sd <- 100*summary.table$sd/summary.table$ap.min.mean
-perc.sum.table$lower.95.bias  <- 100*summary.table$lower.95.bias/summary.table$ap.min.mean
-perc.sum.table$upper.95.bias <- 100*summary.table$upper.95.bias/summary.table$ap.min.mean
-
+perc_sum_table$bias <- 100*summary_table$bias / summary_table$ap_min_mean
+perc_sum_table$sd <- 100*summary_table$sd/summary_table$ap_min_mean
+perc_sum_table$lower_95_bias  <- 100*summary_table$lower_95_bias/summary_table$ap_min_mean
+perc_sum_table$upper_95_bias <- 100*summary_table$upper_95_bias/summary_table$ap_min_mean
 
 # round to 1 digit, arbitrarily
-summary.table[,-1] <- round(summary.table[,-1],1)
-help(par)
+summary_table[,-1] <- round(summary_table[,-1], 1)
+perc_sum_table[,-1] <- round(perc_sum_table[,-1], 1)
+
+write_csv(summary_table,
+          path = "./4_results/summary_table_mins.csv",
+          col_names = T)
+write_csv(perc_sum_table,
+          path = "./4_results/summary_table_perc.csv",
+          col_names = T)
+
+
 # make a plot - on min scale
-plot(1:3,1:3,
-     xlab="",
-     ylab="Bias (95% CI) in minutes (IMGs-AP)",
-     xlim=c(.5,3.5),
-	   ylim=range(summary.table[,c(2,4,5)]),
-	   type="n",axes=F,
-     font.lab=2)
+help(par)
+
+plot(1:3,
+     1:3,
+     xlab = "",
+     ylab = "Bias (95% CI) in minutes",
+     xlim = c(.5,3.5),
+	   ylim = range(summary_table[,c(2,4,5)]),
+	   type = "n",
+     axes = F,
+     font.lab = 2)
+
+title(xlab = "Posture",
+      font.lab = 2,
+      line = 2.5)
+title(sub = "* Mean posture min from AP for sitting = 40.7', standing = 35.1', and movement = 22.5' *", 
+      font.sub = 1,
+      adj = 0.5)
 
 axis(2)
-axis(1,at=1:3,labels=summary.table$posture)
-abline(h=0,lty=3)
-points(1:3,summary.table$bias,pch=16,cex=1.5)
-for (i in 1:3)
-{
-	lines(c(i,i),c(summary.table$lower.95.bias[i],summary.table$upper.95.bias[i]),lty=2)
-	text(i,summary.table$bias[i],paste(summary.table$bias[i],"min"),pos=4,cex=1.25)
+axis(1,
+     at = 1:3,
+     labels = summary_table$posture)
+abline(h = 0,
+       lty = 3)
+points(1:3,
+       summary_table$bias,
+       pch = 16,
+       cex = 1.5)
+
+for (i in 1:3) {
+  
+  lines(c(i,i),
+        c(summary_table$lower_95_bias[i],summary_table$upper_95_bias[i]),
+        lty = 2)
+  text(i,
+       summary_table$bias[i],
+       paste(summary_table$bias[i],"min"),
+       pos = 4,
+       cex = 1.25)
+  
 }
-title(xlab = "Posture", font.lab=2, line = 2.5)
-title(sub = "* Mean posture min from AP for sitting=54.8', standing=26.9', and movement=24.0' *", 
-      font.sub=1,
-      adj=0)
 
 
 # make a plot - on % scale
-plot(1:3,1:3,xlab="Posture",ylab="Bias (95% CI) as %",xlim=c(.5,3.5),
-	ylim=range(perc.sum.table[,c(2,4,5)]),
-	type="n",axes=F)
+plot(1:3,
+     1:3,
+     xlab = "",
+     ylab = "Bias (95% CI) as %",
+     xlim = c(.5,3.5),
+     ylim = range(perc_sum_table[, c(2,4,5)]),
+     type = "n",
+     axes = F,
+     font.lab = 2)
+
+title(xlab = "Posture",
+      font.lab = 2,
+      line = 2.5)
+title(sub = "* Mean posture min from AP for sitting = 40.7', standing = 35.1', and movement = 22.5' *", 
+      font.sub = 1,
+      adj = 0.5)
+
 axis(2)
-axis(1,at=1:3,labels=perc.sum.table$posture)
-abline(h=0,lty=3)
-points(1:3,perc.sum.table$bias,pch=16,cex=1.5)
-for (i in 1:3)
-	lines(c(i,i),c(perc.sum.table$lower.95.bias[i],perc.sum.table$upper.95.bias[i]),lty=2)
+axis(1,
+     at = 1:3,
+     labels = perc_sum_table$posture)
+abline(h = 0,
+       lty = 3)
+points(1:3,
+       perc_sum_table$bias,
+       pch = 16,
+       cex = 1.5)
+
+for (i in 1:3) {
+  
+  lines(c(i,i),
+        c(perc_sum_table$lower_95_bias[i], perc_sum_table$upper_95_bias[i]),
+        lty = 2)
+  text(i,
+       perc_sum_table$bias[i],
+       paste0(perc_sum_table$bias[i],"%"),
+       pos = 4,
+       cex = 1.25)
+  
+}
 
 
