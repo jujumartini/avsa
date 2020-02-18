167e2f72f2573e53caa4de790b970fbbe36895b9
Julian END416 <marti994@uwm.edu>
2020-02-18 14:48:49 -0600

    Clean 0_load.R
    -Took out equivalence package
    -Took out here package
    -added in vroom package

--- 2_scripts/0_load.R
+++ 2_scripts/0_load.R
@@ -3,17 +3,10 @@
 
 # load packages
 library(tidyverse)
+library(vroom)
 library(lubridate)
 library(padr)
-library(equivalence)
 library(irr)
 library(lme4)
-library(here)
 
-# check to see any tidyverse conflicts
 tidyverse_conflicts()
-
-# check working directory. If not using an Rproject, use the here function 
-# before each path
-here()
-
